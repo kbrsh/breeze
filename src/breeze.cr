8fb966d9f51a8172cdd4f3dc38d8f4802eda9021
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 20:25:16 -0800

    two parameters

--- src/breeze.cr
+++ src/breeze.cr
@@ -2,8 +2,7 @@ require "./breeze/*"
 require "http"
 
 module Breeze
-  config = Breeze.config
-  def self.start(port ip)
+  def self.start(port, ip)
     server = HTTP::Server.new(ip, port, handlers)
     server.listen
   end
