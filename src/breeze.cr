3f56a9be66a69e62620c046f795ef0c7dc24a05f
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 20:36:17 -0800

    uncomment new server

--- src/breeze.cr
+++ src/breeze.cr
@@ -5,7 +5,7 @@ module Breeze
   def self.start(port = nil, ip = nil)
     config = Config.new port, ip
     handlers = [] of HTTP::Handler
-    #server = HTTP::Server.new(config.ip, config.port, handlers)
-    #server.listen
+    server = HTTP::Server.new(config.ip, config.port, handlers)
+    server.listen
   end
 end
