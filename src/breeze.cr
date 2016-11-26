44f06d2060d36be2c8f436c3da003044dd0f97a0
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 21:19:19 -0800

    push new handler to handler

--- src/breeze.cr
+++ src/breeze.cr
@@ -5,6 +5,7 @@ module Breeze
   def self.start(port = nil, ip = nil)
     config = Config.new port, ip
     handlers = [] of HTTP::Handler
+    handlers << Breeze::Handler.new
     server = HTTP::Server.new(config.ip, config.port, handlers)
     server.listen
   end
