da7f0a8fae1306f20b145bd5b722db89c709ef89
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 20:13:13 -0800

    default values

--- src/breeze/config.cr
+++ src/breeze/config.cr
@@ -1,8 +1,10 @@
 module Breeze
   class Config
-    def initialize
-      @port = 3000
-      @ip = "0.0.0.0"
+    def initialize(port, ip)
+      @port = port ? port : 3000
+      @ip = ip ? ip : "0.0.0.0"
     end
+
+
   end
 end
