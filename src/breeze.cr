564cc331d673d7375cb2a0516189b8c0dc8ee651
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 20:35:55 -0800

    add handlers

--- src/breeze.cr
+++ src/breeze.cr
@@ -4,7 +4,7 @@ require "http"
 module Breeze
   def self.start(port = nil, ip = nil)
     config = Config.new port, ip
-    puts config.port
+    handlers = [] of HTTP::Handler
     #server = HTTP::Server.new(config.ip, config.port, handlers)
     #server.listen
   end
