a0df42372d713b71d71daba3349b34ae97670b28
Gopal Shah <simplecooldude1@gmail.com>
2016-11-26 12:38:51 -0800

    make a global handler

--- src/breeze.cr
+++ src/breeze.cr
@@ -2,10 +2,11 @@ require "./breeze/*"
 require "http"
 
 module Breeze
+  main_handler = Breeze::Handler.new
   def self.start(port = nil, ip = nil)
     config = Config.new port, ip
     handlers = [] of HTTP::Handler
-    handlers << Breeze::Handler.new
+    handlers << main_handler
     server = HTTP::Server.new(config.ip, config.port, handlers)
     server.listen
   end
