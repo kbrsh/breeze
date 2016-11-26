fc11d9bfa04f60be116bca496969aa4ca907a4aa
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 20:34:48 -0800

    expose

--- src/breeze.cr
+++ src/breeze.cr
@@ -2,9 +2,10 @@ require "./breeze/*"
 require "http"
 
 module Breeze
-  def self.start(port, ip)
-    config = Breeze.Config(port, ip);
-    server = HTTP::Server.new(config.ip, config.port, handlers)
-    server.listen
+  def self.start(port = nil, ip = nil)
+    config = Config.new port, ip
+    puts config.port
+    #server = HTTP::Server.new(config.ip, config.port, handlers)
+    #server.listen
   end
 end
--- src/breeze/config.cr
+++ src/breeze/config.cr
@@ -5,6 +5,13 @@ module Breeze
       @ip = ip ? ip : "0.0.0.0"
     end
 
+    def self.port
+      return @port
+    end
+
+    def self.ip
+      return @ip
+    end
 
     def protocol(ssl)
       return ssl ? "https" : "http"
