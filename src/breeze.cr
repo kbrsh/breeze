9ae64b0a29ed623e5634538605fb0b97fe2abe07
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 20:27:04 -0800

    use config port, config handlers

--- src/breeze.cr
+++ src/breeze.cr
@@ -3,7 +3,8 @@ require "http"
 
 module Breeze
   def self.start(port, ip)
-    server = HTTP::Server.new(ip, port, handlers)
+    config = Breeze.Config(port, ip);
+    server = HTTP::Server.new(config.ip, config.port, handlers)
     server.listen
   end
 end
