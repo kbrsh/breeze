ccb058cc53e1d4ba8387f2bbd984700d6db68511
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 22:49:17 -0800

    delete handler and route

--- src/breeze/handler.cr
+++ src/breeze/handler.cr
@@ -2,16 +2,6 @@ require "http"
 
 module Breeze
   class Breeze::Handler < HTTP::Handler
-    def initialize
-      @routes = [] of Route
-    end
-
-    def call(ctx)
-
-    end
-
-    def add(method, path, &handler : HTTP::Server::Context -> _)
-      @routes << Route.new(method, path, &handler)
-    end
+    
   end
 end
--- src/breeze/route.cr
+++ src/breeze/route.cr
@@ -1,9 +1,5 @@
 module Breeze
   class Breeze::Route
-    getter handler
-    @parts = [] of String
-    def initialize(method, path, &handler : HTTP::Server::Context -> _)
-      @parts = path.split "/"
-    end
+    
   end
 end
