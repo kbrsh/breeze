6eb00b3c38621505854e0cd86ade72ee8cdbf68c
Gopal Shah <simplecooldude1@gmail.com>
2016-11-25 22:02:01 -0800

    take a breeze handler

--- src/breeze/route.cr
+++ src/breeze/route.cr
@@ -2,7 +2,7 @@ module Breeze
   class Breeze::Route
     getter handler
 
-    def initialize(@method, path, @&handler : HTTP::Context -> _)
+    def initialize(@method, path, @&handler : Breeze::Handler -> _)
       @parts = path.split "/"
     end
   end
