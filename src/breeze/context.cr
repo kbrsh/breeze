4ba0a1fce21db1525e47903ac15133b7137825ad
Gopal Shah <simplecooldude1@gmail.com>
2016-11-26 21:20:24 -0800

    get request and response

--- src/breeze/context.cr
+++ src/breeze/context.cr
@@ -1,7 +1,8 @@
 module Breeze
   class Breeze::Context
+    getter request : Request
+    getter response : Response
     def initialize(@request : Request, @response : Response)
-
     end
   end
 end
