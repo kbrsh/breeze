cd31dcab1f4c0be0668e714f257eadb644623754
Gopal Shah <simplecooldude1@gmail.com>
2016-11-26 22:40:19 -0800

    post requests

--- src/breeze/create.cr
+++ src/breeze/create.cr
@@ -1,3 +1,7 @@
 def get(path, &block : HTTP::Server::Context -> String)
   Breeze::Handler::MAIN.add("GET", path, block)
 end
+
+def post(path, &block : HTTP::Server::Context -> String)
+  Breeze::Handler::MAIN.add("POST", path, block)
+end
