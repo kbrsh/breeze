3e0da89662871833c70d94b222d5f1501a462d1d
Gopal Shah <simplecooldude1@gmail.com>
2016-11-26 21:29:07 -0800

    content type or plain text

--- src/breeze/response.cr
+++ src/breeze/response.cr
@@ -1,7 +1,7 @@
 module Breeze
   class Breeze::Response
     def initialize(content_type)
-      @content_type = "text/plain"
+      @content_type = content_type || "text/plain"
     end
 
     def content_type
