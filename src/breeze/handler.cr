a87d84a1538ff3cb1ee3213c38df836a590c1733
Gopal Shah <simplecooldude1@gmail.com>
2016-11-26 18:39:29 -0800

    use context

--- src/breeze/handler.cr
+++ src/breeze/handler.cr
@@ -27,7 +27,7 @@ module Breeze
         puts "\t\e[32mGET\e[0m #{ctx.request.path}"
         ctx.response.status_code = ctx.response.status_code || 200
         ctx.response.headers["Content-Type"] = "text/plain"
-        ctx.response.puts found.handler.call ctx.request, ctx.response
+        ctx.response.puts found.handler.call ctx
       else
         ctx.response.status_code = 404
         ctx.response.headers["Content-Type"] = "text/plain"
