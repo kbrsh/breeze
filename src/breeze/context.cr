d6b97519228d2d180ae77dc8ca954c6ec50a7e99
Gopal Shah <simplecooldude1@gmail.com>
2016-11-26 21:25:28 -0800

    delete context

--- src/breeze/context.cr
+++ src/breeze/context.cr
@@ -1,8 +1,1 @@
-module Breeze
-  class Breeze::Context
-    getter request : Request
-    getter response : Response
-    def initialize(@request : Request, @response : Response)
-    end
-  end
-end
+
